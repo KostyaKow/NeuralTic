f03ff0eb2aa64d30e4180091fb199dcd99444f56
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2017-01-10 00:14:13 -0500

    Adding WebGL drawer.

---
+++ drawing_app/README.md
@@ -0,0 +1,3 @@
+
+Download glUtils and silvester: https://github.com/mdn/webgl-examples/blob/gh-pages/tutorial/sample2/index.html
+
--- drawing_app/index.js
+++ drawing_app/index.js
@@ -1,3 +1,11 @@
+var canvas;
+var gl;
+var squareVerticesBuffer;
+var mvMatrix;
+var shaderProgram;
+var vertexPositionAttribute;
+var perspectiveMatrix;
+
 var numRows = 100; //500;
 var numCols = 250; //800;
 
@@ -13,49 +21,185 @@ function getInitState(numRows, numCols) {
    return state;
 }
 
-function drawInitHtml(state) {
+function initShaders() {
+  var fragmentShader = getShader(gl, "shader-fs");
+  var vertexShader = getShader(gl, "shader-vs");
 
-   var contentData = '';
+  // Create the shader program
 
-   for (var i in state) {
-      var rowState = state[i];
-      var rowHtml = "<div class='row'>";
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
 
-      for (var j in rowState) {
-         var pixStat = rowState[j];
-         var pixId = 'pix-' + i + '-' + j;
-         var pix = "<div class='pix' id='" + pixId + "'>&nbsp;</div>";
-         rowHtml += pix;
-      }
-      rowHtml += '</div>';
+  // If creating the shader program failed, alert
 
-      contentData += rowHtml;
-   }
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    console.log("Unable to initialize the shader program: " + gl.getProgramInfoLog(shader));
+  }
 
-   //return contentData;
-   $('#content').html(contentData);
+  gl.useProgram(shaderProgram);
+
+  vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+  gl.enableVertexAttribArray(vertexPositionAttribute);
+}
+
+
+function getShader(gl, id, type) {
+  var shaderScript, theSource, currentChild, shader;
+
+  shaderScript = document.getElementById(id);
+
+  if (!shaderScript) {
+    return null;
+  }
+
+  theSource = shaderScript.text;
+
+   if (!type) {
+    if (shaderScript.type == "x-shader/x-fragment") {
+      type = gl.FRAGMENT_SHADER;
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+      type = gl.VERTEX_SHADER;
+    } else {
+      // Unknown shader type
+      return null;
+    }
+  }
+  shader = gl.createShader(type);
+
+   gl.shaderSource(shader, theSource);
+
+  // Compile the shader program
+  gl.compileShader(shader);
+
+  // See if it compiled successfully
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.log("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+      return null;
+  }
+
+  return shader;
+}
+
+var horizAspect = 480.0/640.0;
+
+function initBuffers(verts, colors) {
+   var vertices = verts;
+  squareVerticesBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+   ///////////////
+
+
+  squareVerticesColorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
 }
+
+function drawScene() {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
+
+  loadIdentity();
+  mvTranslate([-0.0, 0.0, -6.0]);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+  gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+  setMatrixUniforms();
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+
+function initWebGL(canvas) {
+  gl = null;
+
+  // Try to grab the standard context. If it fails, fallback to experimental.
+  gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
+  // If we don't have a GL context, give up now
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser may not support it.");
+  }
+
+  return gl;
+}
+
+function run() {
+  var vertices = [
+    1.0,  1.0,  0.0,
+    -1.0, 1.0,  0.0,
+    1.0,  -1.0, 0.0,
+    -1.0, -1.0, 0.0
+  ];
+
+   var colors = [
+    1.0,  1.0,  1.0,  1.0,    // white
+    1.0,  0.0,  0.0,  1.0,    // red
+    0.0,  1.0,  0.0,  1.0,    // green
+    0.0,  0.0,  1.0,  1.0     // blue
+  ];
+
+
+   initBuffers(vertices, colors);
+   drawScene();
+
+
+   setTimeout(function() {
+      vertices[1] = 0.9;
+      initBuffers(vertices, colors);
+      drawScene();
+   }, 1000);
+}
+
+function startWebGL() {
+   var canvas = document.getElementById("glcanvas");
+
+   // Initialize the GL context
+   gl = initWebGL(canvas);
+
+   // Only continue if WebGL is available and working
+   if (!gl) {
+      return;
+   }
+
+   // Set clear color to black, fully opaque
+   gl.clearColor(3.0, 0.5, 0.0, 1.0);
+   // Enable depth testing
+   gl.enable(gl.DEPTH_TEST);
+   // Near things obscure far things
+   gl.depthFunc(gl.LEQUAL);
+   // Clear the color as well as the depth buffer.
+   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+   initShaders();
+
+   run();
+}
+
 
 
 
 function init() {
    initMouseDown();
 
-   var content = $('#content');
-
    var initState = getInitState(numRows, numCols);
 
-   drawInitHtml(initState);
+
+   //var width = '640', height = '480';
+   var width = '1280', height = '980';
+
+   $('#glcanvas').attr('width', width);
+   $('#glcanvas').attr('height', height);
 
 
-   $('.pix').each(function() {
-      $(this).mouseleave(function() { //hover or mouseleave
-         if (mouseDown)
-            $(this).stop().css('background-color', 'white');
-      });
-   });
-
+   startWebGL();
 }
 
 $(function() {
@@ -73,4 +217,31 @@ function initMouseDown() {
    }
 
 }
+
+
+
+
+// Matrix utility functions
+//
+
+function loadIdentity() {
+  mvMatrix = Matrix.I(4);
+}
+
+function multMatrix(m) {
+  mvMatrix = mvMatrix.x(m);
+}
+
+function mvTranslate(v) {
+  multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
+}
+
+function setMatrixUniforms() {
+  var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+  gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
+
+  var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+  gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+}
+
 
