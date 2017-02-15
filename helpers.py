ef2439559eb7bdceb2b2b0f26d30142578f79358
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2017-02-15 18:53:01 -0500

    Digit weights calculated correctly

--- helpers.py
+++ helpers.py
@@ -25,6 +25,7 @@ class Neuron:
          self.inputs = inputs
       ret = 0
       for w, b in zip(self.weights, self.inputs):
+         #print('zipping:', w, b)
          ret += w*b
       ret -= self.bias
       #print('perc:', ret)
@@ -34,8 +35,8 @@ class Neuron:
       if inputs is not None:
          self.inputs = inputs
       ret = sigma(self.calculate_perceptron())
-      print('sig:', round(ret, 3))
-      return ret
+      #print('sig:', round(ret, 3))
+      return round(ret, 2)
 
 
 ########digit exercise
@@ -53,33 +54,42 @@ def num_to_input(n):
 
 #print(num_to_input(3))
 
+#weights: 1, 2, 3, 4, 5, 6, 7
+
 def run(num):
    #num = 2 #0, 1, 2
 
    input_num = num_to_input(num)
-   print('input_num:', input_num)
+   #print('input_num:', input_num)
 
    #1/0 (lsb)
-   weights_a = [20, 0, 20] + repeat_num(0, 7) #+ repeat_num(0, 9)
+   weights_a = [20, 0, 20, 0, 20, 0, 20, 0, 20] + repeat_num(0, 7)
    neuron_a = Neuron(weights_a, bias=10)
 
    has_a = neuron_a.calculate(input_num)
 
    #2/0
-   weights_b = [0, 20, 20] + repeat_num(0, 7)
+   weights_b = [0, 20, 20, 0, 0, 20, 20, 0] + repeat_num(0, 7)
    neuron_b = Neuron(weights_b, bias=10)
 
    has_b = neuron_b.calculate(input_num)
 
    #4/0
-   weights_c = [20, 20] + repeat_num(0, 8)
-   neuron_c = None
+   weights_c = [0, 0, 0, 20, 20, 20, 20] + repeat_num(0, 8)
+   neuron_c = Neuron(weights_c, bias=10)
+
+   has_c = neuron_c.calculate(input_num)
 
    #8/0 (msb)
-   neuron_d = None
+   weights_d = repeat_num(0, 7) + [20, 20] + repeat_num(0, 10)
+   neuron_d = Neuron(weights_d, bias=10)
+   has_d = neuron_d.calculate(input_num)
 
 
-   print('input: %i \ta1: %f b2: %f' % (num, has_a, has_b))
+   print('input: %i \ta1: %.2f b2: %.2f c3: %.2f d4: %.2f' % (num, has_a, has_b, has_c, has_d))
+   print('%i = %i' % (num, has_a + has_b*2 + has_c*4 + has_d*8))
 
-
+for i in range(0, 9):
+   run(i)
+#run(5)
 
